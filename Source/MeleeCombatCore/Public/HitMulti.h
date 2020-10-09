3e4574b52f02afb18f43426090ad2bd7067c35af
baihan <bh198@hotmail.com>
2020-10-09 19:43:50 +0800

    Add hit interval

--- Source/MeleeCombatCore/Public/HitMulti.h
+++ Source/MeleeCombatCore/Public/HitMulti.h
@@ -18,6 +18,14 @@ private:
     TArray<AActor *> m_actors;
 
 public:
+    /*hit interval of multi attack*/
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Solution")
+    float m_hitInterval = 0.1f;
+
+private:
+    bool m_isCooling = false;
+
+public:
     virtual void OnHit_Implementation(AActor *attackedActor) override;
 
     virtual void InitData_Implementation() override;
