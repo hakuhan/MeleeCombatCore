383974d68f04e25217c7ab16b0f0de7e099b9777
baihan <bh198@hotmail.com>
2020-12-29 23:48:27 +0800

    Update skill montage Update hurt reaction interface

--- Plugins/MeleeCore/Source/MeleeCore/Public/CombatComponents/MeleeDetection/DetectSolutions/CombatReaction.h
+++ Plugins/MeleeCore/Source/MeleeCore/Public/CombatComponents/MeleeDetection/DetectSolutions/CombatReaction.h
@@ -22,6 +22,6 @@ public:
     void OnMeleeHitted(float hurts);
 
     UFUNCTION(BlueprintNativeEvent)
-    ECombatHitResult HitResult();
+    ECombatHitResult GetHitResult();
 
 };
--- Plugins/MeleeCore/Source/MeleeCore/Public/CombatComponents/MeleeDetection/DetectSolutions/CombatSolution.h
+++ Plugins/MeleeCore/Source/MeleeCore/Public/CombatComponents/MeleeDetection/DetectSolutions/CombatSolution.h
@@ -65,7 +65,7 @@ protected:
             for (auto reacter : reacters)
             {
                 ICombatReaction::Execute_OnMeleeHitted(reacter, m_HurtInfo.hurts * m_HurtRate);
-                outResult = ICombatReaction::Execute_HitResult(reacter);
+                outResult = ICombatReaction::Execute_GetHitResult(reacter);
             }
             // UE_LOG(LogTemp, Warning, TEXT("Multi attack"));
         }
