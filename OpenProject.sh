04c6960c1838a615d69bd45c3b8338669c57ee96
baihan <bh198@hotmail.com>
2021-09-26 17:56:11 +0800

    Update designs

--- OpenProject.sh
+++ OpenProject.sh
@@ -1,1 +1,1 @@
-'/home/bc/Documents/UnrealEngine/Engine/Binaries/Linux/UE4Editor' '/home/bc/Documents/Unreal Projects/MeleeCombatCore/MeleeCombatCore.uproject'
+'/home/bc/UnrealEngine/Engine/Binaries/Linux/UnrealEditor' '/home/bc/文档/Unreal Projects/MeleeCombatCore/MeleeCombatCore.uproject'
