06662d2da73c378b6f283549d72ca3b634b2c6c2
baihan <bh198@hotmail.com>
2021-10-08 17:24:09 +0800

    移动MeleeCore模块为submodule

--- Plugins/MeleeCore/Source/MeleeCore/Private/CombatComponents/MeleeDetection/InvisibleWeapon.cpp
+++
@@ -1,9 +0,0 @@
-#include "MeleeDetection/InvisibleWeapon.h"
-
-void UInvisibleWeapon::BeginPlay()
-{
-	Super::BeginPlay();
-
-	m_Info.SocketNames.Empty();
-	m_Info.SocketNames.Add(m_SocketName);
-}
--- Plugins/MeleeCore/Source/MeleeCore/Private/CombatComponents/MeleeDetection/SkeletalWeapon.cpp
+++
@@ -1,9 +0,0 @@
-#include "MeleeDetection/SkeletalWeapon.h"
-
-void USkeletalWeapon::BeginPlay()
-{
-	Super::BeginPlay();
-
-	m_Info.SocketNames.Empty();
-	m_Info.SocketNames.Append(GetAllSocketNames());
-}
