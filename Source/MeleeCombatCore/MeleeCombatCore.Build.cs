c2a13cde364d8e7248126ade22f06a5f61eec0ac
baihan <bhan198@outlook.com>
2023-08-01 09:48:54 +0800

    upgrade to 5.2

--- Source/MeleeCombatCore/MeleeCombatCore.Build.cs
+++ Source/MeleeCombatCore/MeleeCombatCore.Build.cs
@@ -8,9 +8,9 @@ public class MeleeCombatCore : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AnimGraph", "AnimGraphRunTime", "BlueprintGraph" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AnimGraph", "AnimGraphRuntime", "BlueprintGraph" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraph", "AnimGraphRunTime", "BlueprintGraph" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AnimGraph", "AnimGraphRuntime", "BlueprintGraph" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
